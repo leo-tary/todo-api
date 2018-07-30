34a1d1ab8d83fcb2d64e22e091e03a194ac48ecf
leo_tary <leo.tarun@gmail.com>
2018-07-31 02:36:16 +0530

    Added update document(s)

---
+++ playground/mongodb-update.js
@@ -0,0 +1,34 @@
+const {MongoClient , ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/playground' , { useNewUrlParser: true } , (err , client) => {
+
+    if(err)
+        return console.log(`Error connecting to mongodb...${err}`)
+    
+    console.log(`Connected to mongodb...`);
+
+    const dbName = 'playground';
+    const db = client.db(dbName);
+
+    db.collection('Users').findOneAndUpdate({
+                                                "_id": new ObjectID('5b5f2dfa9f781c373fff9564')
+                                    },
+                                    {
+                                        "$inc":{
+                                            "age":-33
+                                        },
+                                        "$set":{
+                                            "name":"Robo Mongo"
+                                        }
+                                    },
+                                    {"returnOriginal":false}
+                                )
+                                .then((res) => {
+                                    console.log(res);
+                                })
+                                .catch((err) => {
+                                    console.log(`Error updating document....${err}`);
+                                })
+                                
+
+})
